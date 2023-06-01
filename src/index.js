7993b6cb3c35628004f2c68d390cac70b7fca540
Mahir Tekin <humbldump@protonmail.com>
2023-06-01 11:05:39 +0300

    first push

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css'
+import './js'
+
+export default {}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx,css}",
+    ],
+    theme: {
+        extend: {
+
+
+
+            fontFamily: {
+                'poppins': ['Poppins', 'ui-serif', 'Roboto'],
+                'sans': ['ui-sans-serif', 'system-ui'],
+                'serif': ['ui-serif', 'Georgia'],
+                'mono': ['ui-monospace', 'SFMono-Regular'],
+                'display': ['Oswald'],
+                'body': ['Open Sans'],
+            }
+        },
+    },
+    plugins: [],
+}
